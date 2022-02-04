d56e62a42937e8431469b6028a0c9abec8627c19
Rigo <rsarmiento@fivefaces.com.au>
2022-02-04 18:50:41 +0800

    Add exclusion annotation

--- src/main/java/sarmiento/rigo/martrust/ForexApplication.java
+++ src/main/java/sarmiento/rigo/martrust/ForexApplication.java
@@ -2,12 +2,13 @@ package sarmiento.rigo.martrust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ForexApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ForexApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ForexApplication.class, args);
+    }
 
 }
