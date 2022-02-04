af0a136142968115864135a8c1d4c1842d6d84e1
Rigo <rsarmiento@fivefaces.com.au>
2022-02-04 15:59:52 +0800

    Initial commit

---
+++ src/main/java/sarmiento/rigo/martrust/ForexApplication.java
@@ -0,0 +1,13 @@
+package sarmiento.rigo.martrust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sarmiento/rigo/martrust/ForexApplicationTests.java
@@ -0,0 +1,13 @@
+package sarmiento.rigo.martrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
